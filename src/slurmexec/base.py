b5a16e9fa5f25193d40595f81392e52468ee1bcd
Chase King <iamchaseking@gmail.com>
2024-06-11 21:25:10 -0400

    remove default slurm args

--- src/slurmexec/base.py
+++ src/slurmexec/base.py
@@ -217,8 +217,8 @@ def slurm_exec(func, n_parallel_jobs=1, script_dir=None, job_name=None, slurm_ar
 
         # Load slurm batch arguments
         default_slurm_args = {
-            "--ntasks": 1,
-            "--cpus-per-task": 1,
+            # "--ntasks": 1,
+            # "--cpus-per-task": 1,
             # "--mem-per-cpu": "1G"
             # "--time": "1-00:00:00",  # default 1 day
         }
